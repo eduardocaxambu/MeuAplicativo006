2f05a0657e453131c2ad318313f2127b2a577e9e
eduardocaxambutradutor@gmail.com <Eduardo>
2022-08-22 14:37:33 -0300

    Initial commit

---
+++ app/src/main/java/com/eduardocaxambu/meuaplicativo006/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package com.eduardocaxambu.meuaplicativo006.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
